211aedb8c4e10a2cc4df24ae6e7a443dd9874379
janainasouza-dev <janaina.souza@estudante.ifto.edu.br>
2026-03-02 23:51:16 -0300

    finalizando questao 15 e comecando 16

--- src/EstruturaSequecial/Exercicio14.java
+++ src/EstruturaSequecial/Exercicio14.java
@@ -14,7 +14,7 @@ public class Exercicio14 {
         public static void main(String[] args){
         Scanner entrada = new Scanner(System.in);
         System.out.println("Digite a quantidade de gasolina desejada: ");
-        float idade = entrada.nextFloat();
-        System.out.println("Preço total da gasolina: " + idade * 6.30);
+        float litrosGasolina = entrada.nextFloat();
+        System.out.println("Preço total da gasolina: " + litrosGasolina * 6.30);
     }
 }
